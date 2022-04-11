68372f466b3e37c4f98fe87632cbef836748c7bd
jonmgeiger <jonmichaelgeiger@gmail.com>
2022-04-10 20:49:25 -0700

    Add script for dataframe column naming

---
+++ scripts/names_list.R
@@ -0,0 +1,19 @@
+names_list <- c(
+    "school_ID", 
+    "state", 
+    "dist",  # Geographic School District 
+    "children", # Children 5-17 (SAIPE Estimate)
+    "pct_pov", # % Poverty (SAIPE Estimate)
+    "pct_SP", # % Single Parent Estimate
+    "SP_MOE", # Single Parent Margin of Error
+    "pct_HHVJ", # % HHs With Vulnerable Job Estimate
+    "HHVJ_MOE", # Vulnerable Job Margin of Error
+    "pct_CC", # % Crowded Conditions Estimate
+    "CC_MOE", # HH With Crowded Conditions Margin of Error
+    "pct_NCI", # % No Computer or Internet Estimate
+    "nci_MOE", # No Computer or Internet Margin of Error
+    "pct_CD", # % Children with Disability
+    "CD_MOE", # Children with Disability Margin of Error
+    "pct_CLI", # % Linguistically Isolated Children
+    "CLI_MOE" # Linguistically Isolated Children Margin of Error
+)
